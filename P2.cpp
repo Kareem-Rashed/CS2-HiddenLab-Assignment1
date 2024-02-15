d2c3b0eb857617cd5c66facde8fe7c01209a1c6b
KareemRashed <Kareemrashed@aucegypt.edu>
2024-02-15 13:31:05 +0200

    Added Recursion

--- P2.cpp
+++ P2.cpp
@@ -3,34 +3,27 @@
 using namespace std;
 
 
-bool is_Palindrome(string s){
+bool is_Palindrome(string s, int front, int end){
     
-    char flipped[s.length()];
-    int check=0;
-    
-    for(int i = 0; i<s.length(); i++)
-    {
-        flipped[i] = s[s.length()-1-i];
-    }
-    
-    for(int i = 0; i<s.length(); i++)
-    {
-        if(flipped[i] == s[i])
-            check++;
-    }
-    return(check==s.length());
+    if((s[end]==s[front])&&(end <= front))
+        return true;
+    else if (s[end]==s[front])
+        return is_Palindrome(s, front+1, end-1);
+    else
+        return false;
+        
     
 }
 
 
-
+// lol // laal // radar
 int main(){
     
     string word;
     
     cin>>word;
     
-    if(is_Palindrome(word))
+    if(is_Palindrome(word,0,word.length()-1))
         cout<<endl<<"This word is indeed a palindrome"<<endl;
     else
         cout<<endl<<"This word is NOT a palindrome"<<endl;
